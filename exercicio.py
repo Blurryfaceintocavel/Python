bd972dfd389eb19583c053c7e4941e19df3c9a12
Arih <95106440+Blurryfaceintocavel@users.noreply.github.com>
2024-02-07 19:09:09 -0300

    Update exercicio.py

--- exercicio.py
+++ exercicio.py
@@ -1,17 +1,1 @@
-text="Vendo texto"
-name=str(input (f"Escreva seu nome completo: ")) .strip()
-
-print("todas maiusculas: ")
-print(name.upper())
-#
-print("todas minusculas: ")
-print(name.lower())
-#
-print("Quantas letras ao todo (sem considerar espaços).")
-print (len (name))
-
-print("Quantas letras tem o primeiro nome.: ")
-
-
-
 
