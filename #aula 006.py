8d6135842c1ec780c66bc03cbba3abd33151df50
Arih <95106440+Blurryfaceintocavel@users.noreply.github.com>
2024-02-14 19:07:05 -0300

    Add files via upload

---
+++ #aula 006.py
@@ -0,0 +1,12 @@
+#Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.
+
+import math
+n=int (input (f"Digite um número "))
+
+d= n*2
+tr=n*3
+raiz= math.sqrt(n)
+
+print(f" Dobro do n é: {d}")
+print(f" Triplo do n é: {tr}")
+print (f"raiz do n: {raiz}")
---
+++ #aula 007.py
@@ -0,0 +1,7 @@
+#Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
+
+n1=int(input(f" Digite a 1 nota: "))
+n2=int(input(f" Digite a 2 nota: "))
+media=n1+n2/2
+
+print (f"Vc está com média {media}")
