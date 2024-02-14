262d9cbcead160ca03ada4ff23eb5974b94326e4
Arih <95106440+Blurryfaceintocavel@users.noreply.github.com>
2024-02-14 18:30:21 -0300

    Add files via upload

---
+++ #aula 005.py
@@ -0,0 +1,8 @@
+#Faça um programa que leia um número inteiro e 
+#mostre na tela seu antecessor e sucessor
+
+n=int(input(f" Digite um número inteiro: "))
+s=n+1 
+a=n-1
+print (f" O Sucessor do seu número é: {s} ")
+print (f"e seu antecessor é: {a} ")
